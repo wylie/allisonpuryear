5d9a3ee08e3859565ea8831958be4ea90c4bde06
Wylie Fisher <wylie@dukeofcheese.com>
2013-08-06 13:00:10 -0400

    modifications

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,40 +4,29 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		less: {
-			compile: {
-				src: "less/basic-starter.less",
-				dest: "css/basic-starter.css"
+		// less: {
+		// 	compile: {
+		// 		src: "less/basic-starter.less",
+		// 		dest: "css/basic-starter.css"
+		// 	}
+		// },
+		
+		express: {
+			server: {
+				options: {
+					port: 9000,
+					bases: "site"
+				}
 			}
-		},
-		watch: {
-			scripts: {
-				files: ['*.php']
-			}
-		},
-		exec: {
-			serverup: {
-				command: '/Applications/MAMP/bin/start.sh'
-			},
-			serverdown: {
-				command: '/Applications/MAMP/bin/stop.sh'
-			}
-			
 		}
 	});
 
-	grunt.loadNpmTasks("grunt-contrib-less");
-	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.loadNpmTasks('grunt-exec');
+	// grunt.loadNpmTasks("grunt-contrib-less");
+	grunt.loadNpmTasks('grunt-express');
 
 	grunt.registerTask("compile", [
-		"less"
-	]);
-
-	grunt.registerTask('server', [
-		'exec:serverup',
-		'watch',
-		'exec:serverdown'
+		"express",
+		"express-keepalive"
 	]);
 
 	
