c21d320088acd9f9525ee9247bc73793212588af
wylie <wylie@dukeofcheese.com>
2014-08-24 15:40:05 -0400

    merging with remote files

---
+++ site/js/pages.js
@@ -0,0 +1,68 @@
+var pages = {
+	welcome: [
+		'<div id="welcome">',
+			'<img class="img logo" src="img/logo-lg-new.gif" alt="logo" />',
+			'<img class="img main" src="img/hula.jpg" alt="hero" />',
+			'<h1>You can\'t change your life until you start changing your life.</h1>',
+			'<p>Eating Disorders have a way of taking over your life. By the time you\'ve gotten to this website, you\'ve probably tried to stop the obsession on your own. Most people with eating disorders excel in many ways and it\'s hard for them to understand why they can\'t get this under control. Your friends and family, if they know, may not know how to support you.</p>',
+			'<p>I\'m here to help. I want you to be able to smile genuinely when you look in the mirror. I want you to have time and energy to focus on the things in life that matter more to you than food, appearance, and exercise. We can work towards discovering who you are without the eating disorder, and loving that person.</p>',
+		'</div>'
+	],
+	aboutEatingDisorders: [
+		'<div id="about-eating-disorders">',
+			'<img class="img" src="img/trees.jpg" alt="logo" />',
+			'<h1>FAQ\'s and Common Concerns</h1>',
+			'<h2>Why is this happening to me?</h2>',
+			'<p>Unfortunately, there isn\'t a simple answer to this. For some, it\'s obvious: disordered eating behaviors correlate with a traumatic event or long term family issues. Others are unsure why they have eating disorders; their lives have been good and the people in their lives love them. Social pressures and media often exacerbate low self-esteem and negative body image. The onset of mood disorders, such as depression and anxiety, make some people feel out of control. For many, vigilantly attending to food or weight becomes a comfort. Regardless, people with eating disorders all have one thing in common - they have experienced a level of intense emotion and did not know what to do with it. Learning to express emotions appropriately can reduce eating disordered behavior. Identifying the needs the eating disorder is meeting (i.e., control, attention, comfort, self-punishment) and finding alternative ways to meet these needs can also decrease symptoms.</p>',
+			'<h2>I don\'t think it\'s all that deep. I just can\'t seem to get skinny enough that I\'m happy.</h2>',
+			'<p>If eating disorders were really about food and weight, you would be able to stop on your own. You wouldn\'t have fallen into the traps of binge eating, purging, over-exercising and/or restricting. We all know how to lose weight healthily: moderate eating and exercise. Any unhealthy attempt to lose weight indicates a problem with self care. Exploring why you choose not to take care of yourself, or lack the motivation to treat yourself well can be helpful. Obsessing over weight, food, exercise, or appearance can impede your social life, academic or work performance, and quality of life.</p>',
+			'<h2>If I recover, will I get fat?</h2>',
+			'<p>Recovery requires you to take care of your body. That means maintaining a healthy weight through nutritious eating and moderate exercise. I have never worked with someone who took good care of her/his body and became fat.</p>',
+			'<h2>What should I know about recovering from an eating disorder?</h2>',
+			'<p>While you are working on recovery, all of the same external pressures will still be there. You will continue to see people you compare yourself to. Meals will still need to be eaten. People in your life who commented on your appearance before may continue to do so. Your disordered eating has served a purpose and now you will attempt to face these challenges with healthier coping skills. Recovering from any problem is rarely as fluid and easy as we would hope. There are usually "slips" where a person reverts to old behaviors. Instead of seeing these as failure or "relapse", it is helpful to view them as part of the process of healing. No one is perfect and trying to be has made you sick. I will help you navigate this with an eye on progress, not perfection.</p>',
+		'</div>'
+	],
+	aboutTherapy: [
+		'<div id="about-therapy">',
+			'<h1>Psychotherapy that focuses on the therapy part.</h1>',
+			'<p>Research shows that the single most important factor in effective therapy is the relationship between the therapist and the client. My goal is for you to feel supported and respected no matter what you tell me or how you tell me. I will never pry information out of you. I trust you to tell me information you think is important when you feel safe doing so. I welcome any questions, concerns, or dissatisfactions you may have about our work together at any time. My job is to earn your trust.</p>',
+			'<p>Working with me will most likely involve attending to current concerns in your life as well as how those concerns developed in the first place. I believe working at both levels is essential in truly being able to make meaningful and lasting change.</p>',
+			'<p>In the first session I will help you come up with “treatment goals,” which will guide our work together. I typically meet with people weekly at first and taper to every other week as clients begin to heal and their pain subsides.</p>',
+		'</div>'
+	],
+	aboutMe: [
+		'<div id="about-me">',
+			'<img class="img" src="img/headshot.jpg" alt="Allison Puryear" />',
+			'<p>I have been working with people with eating disorders since 2001. After working at an inpatient eating disorder treatment center I coordinated the Eating Disorder Treatment Team within the University of Georgia’s Health Center. I have had private practices in Athens, Georgia, Seattle, Washington & Asheville, North Carolina. I have earned the distinction of being a Certified Eating Disorder Specialist by the International Association of Eating Disorder Professionals.</p>',
+			'<p>I love working with people with eating disorders because they tend to be intelligent, sensitive, driven people who accomplish so much once their anxious energy is channeled in a healthy way. I enjoy working with people in their late teens, twenties, and thirties, because establishing healthy communication and appreciation of self early allows people to enjoy their lives much more. Helping free up my clients over 40 from what has been a lifetime of self-reproach is incredibly rewarding.</p>',
+			'<p>Though I have a specialty in eating disorder treatment, I have experience working with a variety of mental health concerns, especially sexual trauma and anxiety disorders. I have worked with people aged 4 to 66, with at-risk youth and college students, with people with psychosis and those who just feel blue. I have been a forensic interviewer and spent two years working exclusively with survivors of sexual trauma. I have been in the mental health field since 1998. I attended the University of Georgia for both undergrad and graduate school. I am licensed as a Clinical Social Worker in North Carolina and as an Independent Clinical Social Worker in Washington.</p>',
+		'</div>'
+	],
+	appointmentInfo: [
+		'<div id="appointment-info">',
+			'<div class="side clearfix">',
+				'<ul class="contact-info">',
+					'<li><strong>Contact Information</strong></li>',
+					'<li>(828) 333-3654</li>',
+					'<li>96 Central Ave. Suite 7</li>',
+					'<li>Asheville, NC 28801</li>',
+					'<li><a href="mailto:allisonpuryear@icloud.com">allisonpuryear@icloud.com</a></li>',
+				'</ul>',
+			'</div>',
+			'<div>',
+				'<h1>You can\'t change your life until you start changing your life.</h1>',
+				'<p>I am currently an out-of-network provider for most insurance companies. This means that post-session I supply my clients with a receipt which contains all the necessary information for insurance companies. Clients then submit the receipt to their insurance companies for direct reimbursement.</p>',
+				'<p>To determine if you have mental health coverage, the first thing you should do is check with your insurance carrier. Check your coverage carefully and find the answers to the following questions:</p>',
+				'<ol>',
+					'<li>Do I have mental health benefits?</li>',
+					'<li>Do I need pre-authorization?</li>',
+					'<li>How much does my plan cover for an out-of-network provider? (this is usually a percentage)</li>',
+					'<li>What is my deductible and has it been met?</li>',
+					'<li>Is approval/referral required from my primary care physician?</li>',
+				'</ol>',
+				'<p>Please let me know if I can help with this process in any way.</p>',
+				'<p>Sessions are $125. In an effort to make sessions more affordable, I have a few slots for those who cannot afford my full fee. Please let me know if you require a sliding scale fee.</p>',
+			'</div>',
+		'</div>'
+	]
+};
--- site/js/script.js
+++ site/js/script.js
@@ -1,20 +1,35 @@
 $(function() {
-	// start the page off right
-	$('.pg-wrapper').load('pages.html #welcome');
-	// on click load the other pages
-	$(document).on('click', '.nav-item-link', function() {
+	
+	var func = function(pg) {
+		// get the element where we put the stuff
+		var stuff = document.getElementById('content');
+		// remove array commas
+		var pgInfo = pages[pg].join(' ');
+		// add the stuff to the page
+		stuff.innerHTML = pgInfo;
+	}
+	
+	// load the home page stuff			
+	func('welcome');
 
+	// get the clicked on element
+	$('.nav-item-link').on('click', function() {
+		// get the data attribute
+		var pg = $(this).data('name');
+		// pass data attr and pass to function
+		func(pg);
 		$(this).parent('.nav-item').addClass('active').siblings('.nav-item').removeClass('active');
+		window.location.hash = pg;
+	});
 
-		var pgName = $(this).data('name');
-
-		// load the bit for the page
-		$('.pg-wrapper').load('pages.html #' + pgName);
-
-		// add/remove class for the small logo 
-		$('html').removeClass('welcome').addClass('interior');
-		if(pgName === 'welcome') {
-			$('html').addClass('welcome');
+	$('.nav-item-link').on('click', function() {
+		var clicked = $(this).data('name');
+		if(clicked === 'welcome'){
+			$('html').removeClass('interior').addClass('welcome');
+		} else {
+			$('html').removeClass('welcome').addClass('interior');
 		}
 	});
+	
+	
 });
